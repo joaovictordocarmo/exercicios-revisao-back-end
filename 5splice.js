25f0b22d44b403a28bfa704a245ed8157b498390
joao victor <128156864+joaovictordocarmo@users.noreply.github.com>
2024-04-29 09:11:22 -0300

    Add files via upload

---
+++ 5splice.js
@@ -0,0 +1,12 @@
+const nomes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+
+// Ana e Caio mudaram de escola e o Rodrigo entrou nessa sala. Nosso desafio será atualizar a lista de chamada, removendo os nomes de Ana e Caio, e inserindo Rodrigo.
+
+//nomes.splice(indice do elemento que queremos remover, quantidade de elementos que quero remover)
+let nomesatualizado = nomes.splice(1,2)
+nomes.push("Rodrigo")
+console.log(nomes)
+
+//push ou splice
+
+//nomes.splice(indice do elemento que queremos remover, quantidade de elementos que quero remover, se quiser adicionar algo)
